79a9012dccd969b0f422d2309f54183648bc3471
Ryan <lin.ryanm@gmail.com>
2020-09-03 17:47:03 -0700

    First try creating a .json on the main.js

--- back-end/main.js
+++ back-end/main.js
@@ -1,4 +1,5 @@
-/* This is the Main Page
+/* 
+This is the Main Page
 Currently, it only takes you to the sample.js when a get request is
 made for that 
 */
@@ -14,6 +15,17 @@ const test = require('./testlogin.js');
 //This block here will open the main page and display: 'This is the Main Page'
 app.get('/', (req, res) => {
     res.send('This is the Main Page');
+
+})
+
+app.get('/nuton', (req, res) => {
+    //const firstName = req.query.firstName;
+    res.json({
+        UserName: 'Ryan Lin',
+        Password: 'Password',
+        Birthday: 'January 14, 1999',
+        Email: 'lin.ryan99@hotmail.com',
+        PhoneNumber: '604-446-6883'});
 })
 
 //This block opens up url/testlogin and displays 'Test login succesful!'
@@ -21,7 +33,7 @@ app.get(test_login, (req, res) =>{
     res.send(test());
 })
 
+
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`);
 })
-
